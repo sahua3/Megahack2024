75fcbc3fc7ed061fc494ebf9928ebb4a0cd1d78a
sahua3 <148651919+sahua3@users.noreply.github.com>
2024-01-27 23:57:50 +0000

    updated loginform.css

--- megahack2024/src/components/LoginForm/LoginForm.jsx
+++ megahack2024/src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LoginForm.css';
 import { FaPills } from "react-icons/fa6";
+import { FaUser, FaLock } from "react-icons/fa";
 
 const LoginForm = () => {
     return (
@@ -9,20 +10,22 @@ const LoginForm = () => {
                 <h1>Login</h1>
                 <div className="input-box">
                     <input type="text" placeholder='Username' required />
-                    <FaPills />
+                    <FaUser className='icon' />
+                    
                 </div>
                 <div className="input-box">
                     <input type="password" placeholder='Password' required />
+                    <FaLock className='icon' />
                 </div>
 
                 <div className="remember-forgot">
-                    <label><input type="checkbox" />Remember me pwease </label>
+                    <label><input type="checkbox" />Remember me pwease  </label>
                     <a href="#">Forgot password?</a>
-                    <button type="submit">Login</button>
+                </div>
+                <button type="submit">Login</button>
 
                     <div className="register-link">
                         <p> Don't have an account? :3 <a href="#">Register</a></p>
-                    </div>
                 </div>
             </form>
         </div>
